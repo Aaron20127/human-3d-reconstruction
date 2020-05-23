7c86753ae24203e1c8f58d84d15051fbca8c0246
aaron20127 <lwalgorithm@163.com>
2020-05-23 17:04:08 +0800

    add pose.sh

---
+++ experiments/pose.sh
@@ -0,0 +1,19 @@
+cd src
+
+## kp3d with big pose
+python main.py --task pose --subtask pose_70 \
+    --hm_weight 1 --wh_weight 1 --cd_weight 1 --pose_weight 70 --shape_weight 30 --kp2d_weight 2 --kp3d_weight 2\
+    --batch_size_coco 9 --batch_size_lsp 2 --batch_size_hum36m 9 \
+    --val_batch_size_coco 4 \
+    --save_iter_interval 4000 --save_epoch_interval 1 \
+    --val_iter_interval 4000 --val_epoch_interval 1 --val_batch_size_coco 8\
+    --num_iters -1 --log_iters 15 --num_epochs 2 \
+    --gpus 0 \
+    --num_workers 4 \
+    --min_vis_kps 6 --load_min_vis_kps 6\
+    --lr 1.25e-5 \
+    --lr_scheduler_factor 0.978 \
+    --lr_scheduler_patience 200  \
+    --lr_scheduler_threshold 0.01 \
+
+cd ..
